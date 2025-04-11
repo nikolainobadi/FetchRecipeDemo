e1217bee02d4479fb3b6cc4f2ab7cb14a24b2770
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-04-11 16:56:45 -0700

    project setup

---
+++ FetchRecipeDemo/Sources/App/FetchRecipeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeDemoApp.swift
+//  FetchRecipeDemo
+//
+//  Created by Nikolai Nobadi on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
